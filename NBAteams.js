313c1fdd8b1873f737f65b2ffc9640098d25e923
ArtMejia <arteniomejia.student@careerdevs.com>
2022-05-30 22:56:43 -0400

    first commit

---
+++ NBAteams.js
@@ -0,0 +1,9 @@
+const nbaTeamsEast = ['Celtics', 'Knicks', 'Nets', 'Heat', 'Pistons', 'Bulls', 'Bucks', '76ers', 'Magic', 'Cavaliers', 'Raptors', 'Hornets', 'Pacers', 'Wizards', 'Hawks']
+
+for (let i = 0; i < nbaTeamsEast.length; i++) {
+   if(nbaTeamsEast[i].length >= 5) {
+      console.log(nbaTeamsEast[i]); 
+   }
+
+   
+}
